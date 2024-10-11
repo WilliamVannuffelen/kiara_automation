77335fd0ce91b3e2ae9b365deba42b4aea916b7a
William Vannuffelen <williamvannuffelen@gmail.com>
2024-10-11 22:38:23 +0200

    feat: add func to collapse project

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from src.browser.locate import (
     find_work_item,
     test_work_item_exists,
 )
-from src.browser.navigate import open_timesheet_page, expand_project
+from src.browser.navigate import open_timesheet_page, expand_project, collapse_project
 from src.lib.project_helpers import _format_date_silly, _format_timespan_silly
 from src.browser.update import add_work_item_entry, add_new_work_item
 
@@ -42,38 +42,42 @@ async def async_main(work_items: list):
         # navigate to timesheet page and expand relevant project
 
         # TODO: Add validation to see if we're on landing page, skip if not
-        await open_timesheet_page(page, ctx)
+        # await open_timesheet_page(page, ctx)
 
-        # grab task locator
-        task_locator = await get_task_locator(
+        # # grab task locator
+        # task_locator = await get_task_locator(
+        #     page, "CS0126444 - Wonen Cloudzone - dedicated operationeel projectteam"
+        # )
+        # # grab task index
+        # task_index = await get_task_index(task_locator)
+
+        # # open project
+        # await expand_project(
+        #     page, "CS0126444 - Wonen Cloudzone - dedicated operationeel projectteam"
+        # )
+
+        # date_indices = await get_date_column_indices(page)
+
+        # for work_item in work_items:
+        #     log.info(
+        #         f"Processing work item '{work_item["Description"]}' for date '{work_item["Date"]}'"
+        #     )
+        #     log.debug(f"{work_item.items()}.")
+        #     work_item_exists, work_item_index = await test_work_item_exists(
+        #         page, work_item, date_indices, task_index
+        #     )
+
+        #     if work_item_exists:
+        #         await add_work_item_entry(
+        #             page, work_item, date_indices, task_index, work_item_index
+        #         )
+        #     else:
+        #         await add_new_work_item(page, work_item, task_index, date_indices)
+
+        # testing unexpand (?) project
+        await collapse_project(
             page, "CS0126444 - Wonen Cloudzone - dedicated operationeel projectteam"
         )
-        # grab task index
-        task_index = await get_task_index(task_locator)
-
-        # open project
-        await expand_project(
-            page, "CS0126444 - Wonen Cloudzone - dedicated operationeel projectteam"
-        )
-
-        date_indices = await get_date_column_indices(page)
-
-        for work_item in work_items:
-            log.info(
-                f"Processing work item '{work_item["Description"]}' for date '{work_item["Date"]}'"
-            )
-            log.debug(f"{work_item.items()}.")
-            work_item_exists, work_item_index = await test_work_item_exists(
-                page, work_item, date_indices, task_index
-            )
-
-            if work_item_exists:
-                await add_work_item_entry(
-                    page, work_item, date_indices, task_index, work_item_index
-                )
-            else:
-                await add_new_work_item(page, work_item, task_index, date_indices)
-
         await browser.close()
 
 
--- src/browser/navigate.py
+++ src/browser/navigate.py
@@ -1,10 +1,20 @@
+import logging
+
 from playwright.async_api import Page, Locator
 
 import src.browser.locate as locate
+from src.lib.helpers import _terminate_script
 
+log = logging.getLogger(__name__)
 
 async def open_timesheet_page(page: Page, ctx) -> None:
-    await page.get_by_role("cell", name="knop ga verder").locator("a").click()
+    try:
+        await page.get_by_role("cell", name="knop ga verder").locator("a").click()
+        log.info("Navigated to timesheet page.")
+    except Exception as e:
+        log.error("Failed to navigate to timesheet page. Ensure you're on the landing page after ACM/IDM authentication.")
+        log.error(e)
+        _terminate_script(1)
 
 
 async def expand_project(page: Page, search_string: str) -> None:
@@ -12,4 +22,22 @@ async def expand_project(page: Page, search_string: str) -> None:
     await locate.get_task_index(cell_locator)
 
     row_locator = cell_locator.locator("..")
-    await row_locator.get_by_role("cell", name="Expand").get_by_role("img").click()
+    try:
+        await row_locator.get_by_role("cell", name="Expand").get_by_role("img").click()
+        log.info(f"Expanded project '{search_string}'.")
+    except Exception as e:
+        log.error(f"Failed to expand project '{search_string}'.")
+        log.error(e)
+        _terminate_script(1)
+
+async def collapse_project(page: Page, search_string: str) -> None:
+    cell_locator = page.get_by_role("cell", name=search_string, exact=True)
+    await locate.get_task_index(cell_locator)
+
+    row_locator = cell_locator.locator("..")
+    try:
+        await row_locator.get_by_role("cell", name="Collapse").get_by_role("img").click()
+        log.info(f"Collapsed project '{search_string}'.")
+    except Exception as e:
+        log.error(f"Failed to collapse project '{search_string}'.")
+        log.error(e)
